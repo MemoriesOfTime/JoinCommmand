b559047fd9eee3938afd7aa2551703547660ed54
lt-name <572413378@qq.com>
2020-09-15 17:04:36 +0800

    1.0.0

--- src/main/java/cn/lanink/joincommand/JoinCommand.java
+++ src/main/java/cn/lanink/joincommand/JoinCommand.java
@@ -1,8 +1,7 @@
 package cn.lanink.joincommand;
 
 import cn.nukkit.Player;
-import cn.nukkit.Server;
-import cn.nukkit.command.ConsoleCommandSender;
+import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerJoinEvent;
 import cn.nukkit.plugin.PluginBase;
@@ -14,7 +13,7 @@ import java.util.ArrayList;
  */
 public class JoinCommand extends PluginBase implements Listener {
 
-    public static final String VERSION = "?";
+    public static final String VERSION = "1.0.0-SNAPSHOT git-66d6217";
 
     private ArrayList<String> commands;
 
@@ -23,19 +22,22 @@ public class JoinCommand extends PluginBase implements Listener {
         this.saveDefaultConfig();
         this.commands = new ArrayList<>(this.getConfig().getStringList("commands"));
         this.getServer().getPluginManager().registerEvents(this, this);
-        this.getLogger().info(VERSION + " Enable!");
+        this.getLogger().info("Version: " + VERSION);
     }
 
+    @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         if (!this.commands.isEmpty()) {
             this.getServer().getScheduler().scheduleDelayedTask(this, () -> {
-                for (String s : this.commands) {
-                    String[] cmd = s.split("&");
-                    if ((cmd.length > 1) && ("con".equals(cmd[1]))) {
-                        this.getServer().dispatchCommand(this.getServer().getConsoleSender(), cmd[0].replace("@p", player.getName()));
-                    } else {
-                        this.getServer().dispatchCommand(player, cmd[0].replace("@p", player.getName()));
+                if (player.isOnline()) {
+                    for (String s : this.commands) {
+                        String[] cmd = s.split("&");
+                        if ((cmd.length > 1) && ("con".equals(cmd[1]))) {
+                            this.getServer().dispatchCommand(this.getServer().getConsoleSender(), cmd[0].replace("@p", player.getName()));
+                        }else {
+                            this.getServer().dispatchCommand(player, cmd[0].replace("@p", player.getName()));
+                        }
                     }
                 }
             }, 20);
